edd0f851d76794f960c4ec7f6d8fe85a7007a017
John Kapantzakis <kapantzak@gmail.com>
2022-08-18 22:55:34 +0300

    Add animation to main intro text

--- components/Intro.js
+++ components/Intro.js
@@ -3,11 +3,17 @@ import styles from "@/styles/Intro.module.css";
 const Intro = () => {
   return (
     <div>
-      <h1 className={styles.mainText}>Web developer</h1>
-      <p className={styles.subText}>I build things for the web</p>
-      <p className={styles.subText}>
-        Javascript &#8226; React &#8226; Nextjs &#8226; HTML &#8226; CSS
-      </p>
+      <div className={styles.textWrapper}>
+        <h1 className={styles.mainText}>Web developer</h1>
+      </div>
+      <div className={styles.textWrapper}>
+        <p className={styles.subText}>I build things for the web</p>
+      </div>
+      <div className={styles.textWrapper}>
+        <p className={styles.subText}>
+          Javascript &#8226; React &#8226; Nextjs &#8226; HTML &#8226; CSS
+        </p>
+      </div>
     </div>
   );
 };
