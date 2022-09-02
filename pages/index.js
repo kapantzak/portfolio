f80d2e493f72a819fbe3eaabec49313e67c8a9af
John Kapantzakis <kapantzak@gmail.com>
2022-09-03 00:42:54 +0300

    Remove carousel and change intro

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,36 @@
 import styles from "@/styles/Home.module.scss";
-import Carousel from "@/components/Carousel";
+import useScrollPosition from "hooks/useScrollPosition";
 
 const Home = () => {
+  const scrollPosition = useScrollPosition();
+
   return (
     <>
-      <div>
+      <div className={styles.introOuter}>
         <div className={styles.introWrapper}>
           <h1 className={styles.introHeading}>
             <span className={styles.textWeb}>WEB</span>
             <span className={styles.textDeveloper}>DEVELOPER</span>
+            <span className={styles.textName}>My name is John Kapantzakis</span>
           </h1>
+          <div className={styles.progressBarWrapper}>
+            <div
+              className={styles.progressBar}
+              style={{ width: `${scrollPosition / 10}%` }}
+            />
+          </div>
         </div>
         <div style={{ height: "1500px" }}></div>
       </div>
-      <div style={{ backgroundColor: "#fff", color: "#000" }}>
-        {/* <h2 className="sticky" style={{ fontSize: "7vw" }}>
-          I build things for the web
-        </h2> */}
-        <div
-          className="sticky"
-          style={{ display: "flex", justifyContent: "center" }}
-        >
-          <Carousel />
+      <div className={styles.secondSection}>
+        <div className={styles.secondSectionSticky}>
+          <div className={styles.secondSectionFrame}>
+            <h2
+              style={{ fontSize: "5vw", fontWeight: 400, textAlign: "center" }}
+            >
+              I BUILD THINGS FOR THE WEB
+            </h2>
+          </div>
         </div>
         <div style={{ height: "15000px" }}></div>
       </div>
